3fda2a8dbd4eff0ade97e8813396e57f83f82bc1
dantearrighi <dantea@fibertel.com.ar>
2014-08-21 18:50:09 -0300

    Luego de laburar un rato, solo falta televisoorrrrrrr

--- TP1/src/datos/Electrodomestico.java
+++ TP1/src/datos/Electrodomestico.java
@@ -1,9 +1,72 @@
 package datos;
 
 public class Electrodomestico {
-//MARTINNNN GAY
-	//agrego otra garcha la ptua madre que funcione de 1 vez
-	//quiero cmitear algo
-	// desde github funciona
-	//Hola mundo
+	/*Attributes of the laundry machine*/
+	private float preciobase;
+	private String color ;
+	private consumo consumoEnergetico ;
+	private float peso; 
+	protected enum consumo {A,B,C,D,E,F}; 
+	
+	private static String colorDefault = "BLANCO";
+	private static float precioDefault = 10;
+	private static float pesoDefault= 5;
+	private static consumo consumoDefault = consumo.F;
+	
+	
+	
+	/*Fin variables*/
+
+	public float getPreciobase() {
+		return preciobase;
+	}
+	public void setPreciobase(float preciobase) {
+		this.preciobase = preciobase;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public consumo getConsumoEnergetico() {
+		return consumoEnergetico;
+	}
+	public void setConsumoEnergetico(consumo consumoEnergetico) {
+		this.consumoEnergetico = consumoEnergetico;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	/*Default constructor*/
+	public Electrodomestico()
+	{
+		this.setColor(colorDefault);
+		this.setConsumoEnergetico(consumoDefault);
+		this.setPeso(pesoDefault);
+		this.setPreciobase(precioDefault);
+	}
+	/* Constructor whit price y weight*/
+	public Electrodomestico(float pPrecio, float pPeso)	
+	{
+	this();
+	this.setPreciobase(pPrecio);
+	this.setPeso(pPeso);
+	}
+	
+	/*Constructor all attributes*/
+	public Electrodomestico(float pPrecio, float pPeso,String pColor, consumo pConsumo)
+	{	this();
+		this.setColor(pColor);
+		this.setConsumoEnergetico(pConsumo);
+		this.setPeso(pPeso);
+		this.setPreciobase(pPrecio);
+	}
 }
+	
+	
+	
+	
---
+++ TP1/src/negocio/ElectrodomesticoNegocio.java
@@ -0,0 +1,111 @@
+package negocio;
+
+
+public class ElectrodomesticoNegocio {
+	
+	private enum enumColor {ROJO,NEGRO,AZUL,BLANCO,GRIS};
+	
+	protected float precioFinal;
+	
+	private char comprobarConsumoEnergetico(char pLetra)
+	{
+	
+	switch(pLetra)
+	{
+	case 'A': break;
+	case 'B': break;
+	case 'C': break;
+	case 'D': break;
+	case 'E': break;
+	case 'F':break;
+		default: pLetra='F';
+				 break;
+	}
+		return pLetra;
+	}
+	
+	private String comprobarColor(String pColor)
+
+	{
+		String ColorSelect;
+		
+		enumColor color = enumColor.valueOf(pColor);
+		
+		switch(color)
+		{
+		case ROJO: ColorSelect = "ROJO"; 
+			break;
+		case BLANCO: ColorSelect = "BLANCO"; 
+			break;
+		case NEGRO: ColorSelect = "NEGRO"; 
+			break;
+		case AZUL: ColorSelect = "AZUL"; 
+			break;
+		case GRIS: ColorSelect = "GRIS"; 
+			break;
+		default: ColorSelect= "BLANCO";
+			break;
+	
+		}
+		
+		return ColorSelect;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	protected float  precioFinal(char pConsumo, float pPeso, float pPrecioBase)
+	{
+		precioFinal = pPrecioBase;
+		
+		switch(pConsumo)
+		{
+		
+		case 'A': precioFinal= 100; 
+				break;
+		case 'B': precioFinal= 80;
+				break;
+		case 'C': precioFinal= 60;
+				break;
+		case 'D': precioFinal= 50;
+				break;
+		case 'E': precioFinal= 30;
+				break;
+		case 'F': precioFinal= 10;
+				break;
+			default: precioFinal= 10;
+					 break;
+			
+		}
+		
+		if(0<=pPeso && pPeso<=19)
+		{
+			precioFinal+=10;
+		}
+		else if(20<=pPeso && pPeso<=49)
+		{
+			precioFinal+=50;
+		}
+		else if (50<=pPeso && pPeso<=79)
+		{
+			precioFinal+=80;
+			}
+		else if (80<=pPeso)
+		{
+			precioFinal+=100;
+		}
+		
+		
+		
+		return precioFinal;
+		
+	}
+
+
+
+}
